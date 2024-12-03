4247319c404c4cea269159ebc3b40e1ea50520d7
Rob Garner <jukeLTD@proton.me>
2024-12-02 23:43:19 -0500

    fixes to get it working live

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/terminal/', // Add this line to match your GitHub repo name
+  base: '/', // Add this line to match your GitHub repo name
 })
