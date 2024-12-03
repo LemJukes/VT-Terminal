ef94f7da4103bf8d84f5b30d0e4d57266d20417e
Rob Garner <jukeLTD@proton.me>
2024-12-02 23:31:12 -0500

    updated to use custom domain

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/VT-Terminal/', // Add this line to match your GitHub repo name
+  base: '/terminal/', // Add this line to match your GitHub repo name
 })
